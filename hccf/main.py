3c467f9de02304595888136f6024d5fb284218e4
Andrey Kazarinov <airjordan@yandex-team.ru>
2015-05-11 20:09:27 +0300

    feature: packaging

---
+++ hccf/main.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from argparse import ArgumentParser
+import logging
+import os.path
+
+from hccf.clustering import FeatureClustering
+from hccf.utils.tools import get_parser, get_composer
+from hccf.settings import LOGGING
+from hccf.utils.logs import load_dict_config
+
+
+FORMATS = ['vw', 'libffm']
+DEFAULT_FORMAT = 'vw'
+
+
+def cluster(input, model, format, features, slices, processes=1):
+    fc = FeatureClustering(processes)
+    parser = get_parser(format)
+    fc.cluster(
+        input_logfile=input,
+        tree_features=features,
+        slice_features=slices,
+        parser=parser,
+    )
+    fc.save(model)
+
+
+def convert(input, output, model, type, levels, parser, composer):
+    fc = FeatureClustering.load(model)
+    parser = get_parser(parser)
+    composer = get_composer(composer)
+    output_params = {
+        'type': type,
+        'levels': levels,
+    }
+    fc.convert_log(
+        input_logfile=input,
+        output_logfile=output,
+        output_params=output_params,
+        parser=parser,
+        composer=composer,
+    )
+
+
+def main():
+    argparser = ArgumentParser(description='Agglomerative Hierarchical Clustering')
+    subparsers = argparser.add_subparsers(help='sub-command help', dest='command')
+
+    # clustering command
+    parser_cluster = subparsers.add_parser('cluster', help='clustering')
+    parser_cluster.add_argument('--debug', help="debug mode", action='store_true')
+    parser_cluster.add_argument('-p', '--processes', dest="processes", help="number of processes", type=int, default=1)
+    parser_cluster.add_argument(
+        '--format',
+        dest='format',
+        help="format of input and output file",
+        default=DEFAULT_FORMAT,
+        choices=FORMATS,
+    )
+    parser_cluster.add_argument('-i', '--input', dest="input", help="input file", required=True)
+    parser_cluster.add_argument('-m', '--model', dest="model", help="model file", required=True)
+    parser_cluster.add_argument(
+        '-f', '--features',
+        dest='features',
+        help='features to clustering',
+        nargs='*',
+        required=True,
+    )
+    parser_cluster.add_argument(
+        '-s', '--slices',
+        dest='slices',
+        help='slices for clustering',
+        nargs='*',
+    )
+
+    # convert command
+    parser_convert = subparsers.add_parser('convert', help='convert')
+    parser_convert.add_argument('--debug', help="debug mode", action='store_true')
+    parser_convert.add_argument('-i', '--input', dest="input", help="input file", required=True)
+    parser_convert.add_argument('-o', '--output', dest="output", help="output file", required=True)
+    parser_convert.add_argument(
+        '-t', '--type',
+        dest="type",
+        help="convert type",
+        default='full_tree',
+        choices=['full_tree', 'tree_without_leaves', 'part_tree'],
+    )
+    parser_convert.add_argument('-l', '--levels', dest="levels", help="levels", default=None)
+    parser_convert.add_argument('-m', '--model', dest="model", help="model file", required=True)
+    parser_convert.add_argument(
+        '--parser',
+        dest='parser',
+        help="parser of input file",
+        default=DEFAULT_FORMAT,
+        choices=FORMATS,
+    )
+    parser_convert.add_argument(
+        '--composer',
+        dest='composer',
+        help="composer of output file",
+        default=DEFAULT_FORMAT,
+        choices=FORMATS,
+    )
+
+    options = argparser.parse_args()
+    logging_level = logging.DEBUG if options.debug else logging.INFO
+    load_dict_config(LOGGING, logging_level)
+
+    if options.command == 'cluster':
+        if not os.path.isfile(options.input):
+            argparser.error('no input file `%s` found' % options.input)
+
+        cluster(
+            input=options.input,
+            model=options.model,
+            format=options.format,
+            features=options.features,
+            slices=options.slices,
+            processes=options.processes,
+        )
+    elif options.command == 'convert':
+        if not os.path.isfile(options.model):
+            argparser.error('no model file `%s` found' % options.model)
+
+        convert(
+            input=options.input,
+            output=options.output,
+            model=options.model,
+            type=options.type,
+            levels=options.levels,
+            parser=options.parser,
+            composer=options.composer,
+        )
+    else:
+        argparser.error('no command `%s` found' % options.command)
+
+
+if __name__ == '__main__':
+    main()
--- hccf/setup.py
+++ hccf/setup.py
@@ -3,10 +3,21 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='cfdr-clustering',
+    name='hccf',
+    description="Hierarchical Clustering of Categorical Features",
+    license="MIT",
+    version="0.1",
+    url="https://github.com/kazarinov/hccf",
     author='Andrey Kazarinov',
     author_email='andrei.kazarinov@gmail.com',
+    keywords=["hierarchical clustering", "dimensionality reduction"],
+
     packages=find_packages(exclude=('tests', 'tests.*')),
     install_requires=open('./requirements.txt').read(),
-    scripts=['bin/clustering']
+    entry_points={
+        "console_scripts": [
+            "hccf = hccf.main:main"
+        ]
+    },
+
 )
